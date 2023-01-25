fbfe012e699b23c5396594a239732a44588f3296
EslamElsebaey <eslamelsebaey.11@gmail.com>
2023-01-25 11:16:38 +0200

    edit

--- js/counter.js
+++ js/counter.js
@@ -1,20 +1,20 @@
 
 // counters
 
-const numbers = document.querySelectorAll(".clients-exp h4"); 
+const numbers = document.querySelectorAll(".clients-exp h4");
 const counters = Array(numbers.length);
 const intervals = Array(numbers.length);
 counters.fill(0);
 
 numbers.forEach((number, index) => {
   intervals[index] = setInterval(() => {
-    if (counters[index] == parseInt(number.dataset.num)) {
+    counters[0] += 1;
+    counters[1] += 1000;
+    if (counters[index] >= parseInt(number.dataset.num)) {
       clearInterval(intervals[index]);
+      number.innerHTML = parseInt(number.dataset.num);
     } else {
-      counters[0] += 1;
-      counters[1] += 1000;
       number.innerHTML = counters[index];
-      console.log(counters[index])
     }
-  }, number.dataset.time )
+  }, number.dataset.time);
 });

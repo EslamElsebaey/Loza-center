cffdbd598c482bd9e4096e88b691652528aa2106
EslamElsebaey <eslamelsebaey.11@gmail.com>
2023-06-12 17:13:55 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -202,11 +202,39 @@ $(".li-drop > a").click(function (e) {
   })
 }
 
+
+
+// counters
+
+
+if($(".clients-exp h4").length > 0) {
+  const numbers = document.querySelectorAll(".clients-exp h4");
+  const counters = Array(numbers.length);
+  const intervals = Array(numbers.length);
+  counters.fill(0);
+  
+  numbers.forEach((number, index) => {
+    intervals[index] = setInterval(() => {
+      counters[0] += 1;
+      counters[1] += 1000;
+      if (counters[index] >= parseInt(number.dataset.num)) {
+        clearInterval(intervals[index]);
+        number.innerHTML = parseInt(number.dataset.num);
+      } else {
+        number.innerHTML = counters[index];
+      }
+    }, number.dataset.time);
+  });
+}
+
+
+
+
+
+
 })
 
 /*******************************************************************************************/
-
-
 
 
 
